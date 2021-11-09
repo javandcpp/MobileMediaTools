02149f0b53df556384f7ee73a81ca39e3d58b74f
loneswordman <756280764@qq.com>
2021-11-09 11:44:43 +0800

    更新IOS

---
+++ ios/avcapture/HWCAVCapture.h
@@ -0,0 +1,16 @@
+//
+//  HWCAVCapture.h
+//  Test
+//
+//  Created by developer on 2021/11/9.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HWCAVCapture : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ios/avcapture/HWCAVCapture.m
@@ -0,0 +1,12 @@
+//
+//  HWCAVCapture.m
+//  Test
+//
+//  Created by developer on 2021/11/9.
+//
+
+#import "HWCAVCapture.h"
+
+@implementation HWCAVCapture
+
+@end
